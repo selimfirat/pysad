7c1af1b13dd85f02a3edc2a1559ac49460d35808
selimfirat <yilmazselimfirat@gmail.com>
2020-06-10 06:26:44 +0300

    deviation based models.

---
+++ models/median_absolute_deviation.py
@@ -0,0 +1,34 @@
+from models.base_model import BaseModel
+from stats.average_meter import AverageMeter
+from stats.median_meter import MedianMeter
+from stats.variance_meter import VarianceMeter
+
+
+class MedianAbsoluteDeviation(BaseModel):
+
+    def __init__(self, absolute=True, b=1.4826, **kwargs):
+        """
+        Mean Absolute deviation described in https://en.wikipedia.org/wiki/Median_absolute_deviation and https://arxiv.org/pdf/1704.07706.pdf
+        See https://arxiv.org/pdf/1704.07706.pdf for b parameter selection. b does not affect scores.
+        :param kwargs:
+        """
+        super().__init__(**kwargs)
+
+        self.b = b
+        self.absolute = absolute
+        self.median_meter = MedianMeter()
+        self.mad_meter = MedianMeter()
+
+    def fit_partial(self, x, y=None):
+        assert len(x) == 1 # Only for time series
+
+        self.median_meter.update(x)
+        self.mad_meter.update(x)
+
+    def score_partial(self, x):
+
+        median = self.median_meter.get()
+        mad = self.b*self.mad_meter.get()
+        score = (x - median)/mad
+
+        return abs(score) if self.absolute else score
---
+++ models/standard_absolute_deviation.py
@@ -0,0 +1,38 @@
+from models.base_model import BaseModel
+from stats.average_meter import AverageMeter
+from stats.median_meter import MedianMeter
+from stats.variance_meter import VarianceMeter
+
+
+class StandardAbsoluteDeviation(BaseModel):
+
+    def __init__(self, substracted_statistic="mean", absolute=True, **kwargs):
+        """
+        3-Sigma rule described in https://arxiv.org/pdf/1704.07706.pdf
+        :param kwargs:
+        """
+        super().__init__(**kwargs)
+
+        self.absolute = absolute
+        self.variance_meter = VarianceMeter()
+
+        if substracted_statistic == "median":
+            self.sub_meter = MedianMeter()
+        elif substracted_statistic == "mean":
+            self.sub_meter = AverageMeter()
+        else:
+            raise ValueError("Unknown substracted_statistic value! Please choose median or mean.")
+
+    def fit_partial(self, x, y=None):
+        assert len(x) == 1 # Only for time series
+
+        self.variance_meter.update(x)
+        self.sub_meter.update(x)
+
+    def score_partial(self, x):
+        sub = self.sub_meter.get()
+        dev = self.variance_meter.get()**0.5
+
+        score = (x - sub) / dev
+
+        return abs(score) if self.absolute else score

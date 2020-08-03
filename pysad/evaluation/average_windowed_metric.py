3d5fb6cb24ce51eed15e7f56ee42dd0011af440c
selimfirat <yilmazselimfirat@gmail.com>
2020-08-03 13:53:47 +0300

    Changed windowed s

--- pysad/evaluation/average_windowed_metric.py
+++ pysad/evaluation/average_windowed_metric.py
@@ -3,9 +3,20 @@ from pysad.stats.average_meter import AverageMeter
 
 
 class WindowedMetric(BaseMetric):
+    """A helper class to evlauate windowed metrics. The distributions of the streaming model scores often change due to model collapse (i.e. becoming closer to the always loss=0) or appearing nonstationarities. Thus, the metrics such as ROC or AUC scores may change drastically. To prevent their effect, this class creates windows of size `window_size`. After each `window_size`th object, a new instance of the `metric_cls` is being created. Lastly, the metrics from all windows are averaged :cite:`xstream,gokcesu2017online`.
 
-    def __init__(self, metric_cls, window_size, ignore_nonempty_last=True, **kwargs):
-        super().__init__(**kwargs)
+    Args:
+        metric_cls: class
+            The metric class to be windowed.
+        window_size: int
+            The window size.
+        ignore_nonempty_last: bool
+            Whether to ignore the score of the nonempty last window. Note that the empty last window is always ignored.
+
+    """
+
+    def __init__(self, metric_cls, window_size, ignore_nonempty_last=True):
+        super().__init__()
 
         self.ignore_nonempty_last = ignore_nonempty_last
         self.window_size = window_size
@@ -19,12 +30,23 @@ class WindowedMetric(BaseMetric):
 
         self.num_windows = 0
 
-    def init_metric(self):
+    def _init_metric(self):
 
-        return self.metric_cls(**self.kwargs)
+        return self.metric_cls()
 
     def update(self, y_true, y_pred):
+        """
 
+        Args:
+            y_true: float
+                The ground truth score for the incoming instance.
+            y_pred: float
+                The predicted score for the incoming instance.
+
+        Returns:
+            self: object
+                The updated metric.
+        """
         self.step += 1
         self.metric.update(y_true, y_pred)
 
@@ -32,12 +54,17 @@ class WindowedMetric(BaseMetric):
             self.num_windows += 1
             score = self.metric.get()
             self.score_meter.update(score)
-            self.metric = self.init_metric()
+            self.metric = self._init_metric()
 
         return self
 
     def get(self):
+        """Obtains the averaged score.
 
+        Returns:
+            current_score: float
+                The average score of the windows.
+        """
         if self.ignore_nonempty_last:
             return self.score_meter.get()
         else:

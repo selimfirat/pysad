Change variance formula. Temporarily fixed min, median and max meters. Added test for statistics with numpy batch checkers.

pysad/stats/max_meter.py
@@ -1,6 +1,7 @@
 import math
 from heapq import heappush
 from pysad.core.base_statistic import UnivariateStatistic
+import numpy as np
 
 
 class MaxMeter(UnivariateStatistic):
@@ -50,7 +51,7 @@ class MaxMeter(UnivariateStatistic):
         self.lst.remove(num)
 
         if len(self.lst) > 0:
-            self.max = self.lst[-1]
+            self.max = np.max(self.lst)
         else:
             self.max = -math.inf
 

pysad/stats/min_meter.py
@@ -1,6 +1,7 @@
 import math
 from heapq import heappush
 from pysad.core.base_statistic import UnivariateStatistic
+import numpy as np
 
 
 class MinMeter(UnivariateStatistic):
@@ -52,7 +53,7 @@ class MinMeter(UnivariateStatistic):
         self.lst.remove(num)
 
         if len(self.lst) > 0:
-            self.min = self.lst[0]
+            self.min = np.min(self.lst)
         else:
             self.min = math.inf
 
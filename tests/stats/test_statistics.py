be0d4866e1cf0f104b48ad33d48bb8c2561c80d9
selimfirat <yilmazselimfirat@gmail.com>
2020-08-05 22:56:23 +0300

    Included running statistic to the tests.

--- tests/stats/test_statistics.py
+++ tests/stats/test_statistics.py
@@ -1,8 +1,8 @@
-from pysad.stats.abs_statistic import AbsStatistic
-
 
 def test_all_zero_stats():
     import numpy as np
+    from pysad.stats.abs_statistic import AbsStatistic
+    from pysad.stats.running_statistic import RunningStatistic
     from pysad.stats.average_meter import AverageMeter
     from pysad.stats.count_meter import CountMeter
     from pysad.stats.max_meter import MaxMeter
@@ -27,15 +27,19 @@ def test_all_zero_stats():
     for stat_cls, val in stat_classes.items():
         stat = stat_cls()
         abs_stat = AbsStatistic(stat_cls)
+        window_size = 25
+        running_stat = RunningStatistic(stat_cls, window_size=window_size)
         arr = np.zeros(num_items, dtype=np.float)
         prev_value = 0.0
         for i in range(arr.shape[0]):
             num = arr[i]
             stat.update(num)
             abs_stat.update(num)
+            running_stat.update(num)
             if i > 1: # for variance meter.
                 assert np.isclose(stat.get(), val if val != "count" else i+1)
                 assert np.isclose(abs_stat.get(), val if val != "count" else i+1)
+                assert np.isclose(running_stat.get(), val if val != "count" else min(i+1, window_size))
 
                 stat.remove(num)
                 abs_stat.remove(num)

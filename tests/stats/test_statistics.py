d33f256c753e2125a25907a2c48ac63168ad332f
selimfirat <yilmazselimfirat@gmail.com>
2020-08-05 22:50:58 +0300

    Included abs statistic to the tests.

--- tests/stats/test_statistics.py
+++ tests/stats/test_statistics.py
@@ -1,3 +1,5 @@
+from pysad.stats.abs_statistic import AbsStatistic
+
 
 def test_all_zero_stats():
     import numpy as np
@@ -24,16 +26,22 @@ def test_all_zero_stats():
 
     for stat_cls, val in stat_classes.items():
         stat = stat_cls()
+        abs_stat = AbsStatistic(stat_cls)
         arr = np.zeros(num_items, dtype=np.float)
         prev_value = 0.0
         for i in range(arr.shape[0]):
             num = arr[i]
             stat.update(num)
-            if i > 1:
+            abs_stat.update(num)
+            if i > 1: # for variance meter.
                 assert np.isclose(stat.get(), val if val != "count" else i+1)
+                assert np.isclose(abs_stat.get(), val if val != "count" else i+1)
 
                 stat.remove(num)
+                abs_stat.remove(num)
                 assert np.isclose(stat.get(), prev_value)
+                assert np.isclose(abs_stat.get(), abs(prev_value))
                 stat.update(num)
+                abs_stat.update(num)
 
             prev_value = stat.get()

ec939adf368dcddab1c2e98d8de516096516fa52
selimfirat <yilmazselimfirat@gmail.com>
2020-08-13 06:21:34 +0300

    Disabled showing version in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,7 @@ html_theme_options = {
     'canonical_url': '',
     'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
     'logo_only': True,
-    'display_version': True,
+    'display_version': False,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     #'vcs_pageview_mode': '',
